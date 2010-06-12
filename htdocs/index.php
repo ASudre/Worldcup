d934235ba5dc8628e2d9a87b47502841a5d5e641
Aaron <me@aaronbassett.com>
2010-06-12 23:29:08 +0100

    First commit

---
+++ htdocs/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+
+<meta charset=utf-8>
+
+<title>Worldcup mash. Powered by Kwwika (sponsored by TellyLinks).</title>
+
+<meta name="rating" content="General" />
+<meta name="distribution" content="Global" />
+
+<meta name="googlebot" content="index,follow" />
+<meta name="robots" content="all,index,follow" />
+<meta name="msnbot" content="all,index,follow" />
+
+<meta name="resource-type" content="document" />
+<meta name="revisit-after" content="10 Days" />
+
+<!--[if IE]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<link rel="stylesheet" type="text/css" media="screen" href="/styles/HTML5Reset.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="/styles/worldcup.css" />
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+
+</head>
+<body>
+
+
+
+<script type="text/javascript" src="/js/prototype.js"></script>
+<script type="text/javascript" src="/js/worldcup.js"></script>
+
+</body>
+</html>
