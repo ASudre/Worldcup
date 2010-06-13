4f611a5b5442a842e78480786e058cd51d331577
Aaron <me@aaronbassett.com>
2010-06-13 14:18:31 +0100

    Added Konami easter egg

---
+++ htdocs/js/fifa.js
@@ -0,0 +1,10 @@
+function developersdevelopersdevelopers() {
+	var vidEmbed = '<object type="application/x-shockwave-flash" data="http://www.youtube.com/v/PFv-1t6HUQs&autoplay=1" width="480" height="385">';
+	vidEmbed += '<param name="movie" value="http://www.youtube.com/v/PFv-1t6HUQs&autoplay=1" />';
+	vidEmbed += '<param name="wmode" value="transparent">';
+	vidEmbed += '</object>';
+	jQuery.facebox(vidEmbed);
+}
+konami = new Konami()
+konami.code = developersdevelopersdevelopers
+konami.load();
