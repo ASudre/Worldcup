caba6fcfdd4cdd4fb0e31740a042534cff7f92c6
Aaron <me@aaronbassett.com>
2010-06-14 11:56:00 +0100

    Easter egg now has 100% more Eat my Goal!

--- htdocs/js/fifa.js
+++ htdocs/js/fifa.js
@@ -1,10 +1,20 @@
-function developersdevelopersdevelopers() {
-	var vidEmbed = '<object type="application/x-shockwave-flash" data="http://www.youtube.com/v/PFv-1t6HUQs&autoplay=1" width="480" height="385">';
-	vidEmbed += '<param name="movie" value="http://www.youtube.com/v/PFv-1t6HUQs&autoplay=1" />';
+function GOOOOOOAAAAALLLLL() {
+	var vidEmbed = '<object type="application/x-shockwave-flash" data="http://www.youtube.com/v/0ZhysyhUL9k&autoplay=1" width="480" height="385">';
+	vidEmbed += '<param name="movie" value="http://www.youtube.com/v/0ZhysyhUL9k&autoplay=1" />';
 	vidEmbed += '<param name="wmode" value="transparent">';
 	vidEmbed += '</object>';
-	jQuery.facebox(vidEmbed);
+	
+	var d=document.createElement('div');
+	d.id = "KonamiOverlay";
+	document.body.appendChild(d);
+	$(d).update(vidEmbed);
+	
+	document.body.observe('click', function() {
+		$('KonamiOverlay').remove();
+		document.body.stopObserving('click');
+	})
+	
 }
 konami = new Konami()
-konami.code = developersdevelopersdevelopers
+konami.code = GOOOOOOAAAAALLLLL;
 konami.load();
