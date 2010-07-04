47e553b2b77c302b97d2b235f1ca8585baac1469
Aaron <me@aaronbassett.com>
2010-07-04 15:44:53 +0100

    Add Url.Parse library

--- htdocs/index.php
+++ htdocs/index.php
@@ -263,6 +263,7 @@
 <script type="text/javascript" src="http://www.google.com/jsapi"></script>
 <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;sensor=false&amp;key=ABQIAAAAsqqbEkxZVbQW3Zu5vospYBTyVRo1s37xKwjrEyJXOrfxvvfVRRRrOOyZhTlIKP_yGXfhOahvXQcaDg" type="text/javascript"></script>
 <script type="text/javascript" src="/js/prototype.s2.min.js"></script>
+<script type="text/javascript" src="/js/Url.Parse.js"></script>
 <script type="text/javascript" src="/js/chances.js"></script>
 <script type="text/javascript" src="/js/worldcup.js"></script>
 
